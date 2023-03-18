f5da5d2ad5c70ec3c6602f860ed6d9606dd388b3
prem <premambre80@gmail.com>
2023-03-18 16:07:24 +0530

    refact : removed lint

--- src/views/LogInAndSignUp.vue
+++ src/views/LogInAndSignUp.vue
@@ -2,9 +2,30 @@
 	<div class="login-wrappper">
 		<div class="login-container">
 			<div class="login-image"></div>
-			<div class="login-form">
-				<v-text-field v-model="username" solo label="username" clearable></v-text-field>
-				<v-text-field v-model="password" solo label="password" clearable></v-text-field>
+			<div class="login-form-container">
+				<div class="login-welcome-text">
+					<h2 class="login-text-header">Welcome Back!</h2>
+					<div class="login-text-desc">Enter your username and password to continue</div>
+				</div>
+				<div class="login-form">
+					<v-text-field
+						v-model="username"
+						outlined
+						label="username"
+						placeholder="username"
+						clearable
+					></v-text-field>
+					<v-text-field
+						v-model="password"
+						outlined
+						label="password"
+						placeholder="password"
+						clearable
+					></v-text-field>
+					<div class="login-btn-container">
+						<v-btn class="login-btn" block large elevation="0"> Login </v-btn>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -25,16 +46,25 @@ export default {
 	height: 100vh;
 	width: 100vw;
 }
-.login-container {
+
+.login-form-container {
 	width: 100%;
 	height: 100vh;
 	display: flex;
+	flex-direction: column;
+	align-items: center;
+	overflow: hidden;
 	justify-content: center;
 	align-items: center;
 }
 
 .login-form {
-	height: 100%;
+	width: 100%;
+	padding: 2rem;
+}
+
+.login-btn-container {
+	height: 2rem;
 	width: 100%;
 }
 </style>
