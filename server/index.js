7b394a26883c037629a606a551a402c22e251e96
prem <premambre80@gmail.com>
2023-03-18 13:22:01 +0530

    add : added cors to run cross origin server for backend and front end

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const app=express();
-const mongoose=require('mongoose');
+/* eslint-disable prettier/prettier */
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const app = express()
+app.use(cors())
+const mongoose = require("mongoose")
+const { response } = require("express")
 
-app.listen('5000',function(err)
-{
-    if(err) console.log(err);
-    else console.log('listening on port 5000');
-});
+app.get("/", function (req, res) {
+	res.status(200).json("hii")
+	return;
+})
+
+app.listen("5000", function (err) {
+	if (err) console.log(err)
+	else console.log("listening on port 5000")
+})
