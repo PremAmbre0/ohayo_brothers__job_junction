c4234fe176a23624ad1ac5fb56deeb8e8715bfb1
prem <premambre80@gmail.com>
2023-03-18 15:15:51 +0530

    refact lint and deleted reduntant files

---
+++ src/views/LogInAndSignUp.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="login-wrappper">
+		<div class="login-container">
+			<div class="login-image"></div>
+			<div class="login-form">
+				<v-text-field v-model="username" solo label="username" clearable></v-text-field>
+				<v-text-field v-model="password" solo label="password" clearable></v-text-field>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "loginAndSignup",
+	data: () => ({
+		username: "",
+		password: "",
+	}),
+}
+</script>
+
+<style lang="scss" scoped>
+.login-wrappper {
+	height: 100vh;
+	width: 100vw;
+}
+.login-container {
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.login-form {
+	height: 100%;
+	width: 100%;
+}
+</style>
